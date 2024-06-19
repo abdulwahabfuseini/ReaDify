e6b50248e64e7c147a0cb0c6706b5a5abdc27e27
abdulwahabfuseini <abdulwahabfuseini78@gmail.com>
2024-06-19 02:07:34 +0000

    Vercel Error Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <Providers>
             <ToastContext />
             {children}
-            <ChatBot />
+            <ChatBot show={false} />
           </Providers>
         </Loading>
       </body>
